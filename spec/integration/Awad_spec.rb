b97f7d59047ed8e09f706fb15882e26402c233d7
Charalamboss <ch.pouroutides@gmail.com>
2018-11-22 20:08:35 +0200

    add search flight

--- spec/integration/Awad_spec.rb
+++ spec/integration/Awad_spec.rb
@@ -7,3 +7,18 @@ feature 'create customer' do
         expect(page).to have_content('Name')
     end
 end
+feature 'administrors add more airpirts' do
+	scenario "csv import" do
+		visit '/airports/new'
+		expect(page).to have_content(Import)
+		click_button'Add Airports'
+		#https://www.youtube.com/watch?v=W8pohTautj8
+	end
+end
+
+feature 'search for flight' do
+	scenario "Auto complete search" do
+		visit '/quesry/new' 
+		fill_in 'Destination', :with=>'LHR'
+	end
+end
